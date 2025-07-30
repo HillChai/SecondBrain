64118e24c93ea91389f6fc75c74007926016384a
Chongcong Chai <chaichongcong@126.com>
2025-07-30 21:58:34 +0800

    Initial Commit

---
+++ SecondBrain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SecondBrain
+//
+//  Created by Cosen Chai on 2025/7/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SecondBrain/SecondBrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondBrainApp.swift
+//  SecondBrain
+//
+//  Created by Cosen Chai on 2025/7/30.
+//
+
+import SwiftUI
+
+@main
+struct SecondBrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
